2ca395897f514b35b56cea65f26652e7e1ddddf3
alexg16 <alexgrinberg@msn.com>
2024-01-08 13:38:31 -0500

    Update git-code-hugo.ps1

--- git-code-hugo.ps1
+++ git-code-hugo.ps1
@@ -1,3 +1,3 @@
-winget install git.git -e --silent --accept-source-agreements --accept-package-agreements
+winget install git.git --silent --accept-source-agreements --accept-package-agreements
 winget install -e --silent --accept-source-agreements --accept-package-agreements --id Microsoft.VisualStudioCode
 winget install hugo.hugo --silent --accept-source-agreements --accept-package-agreements
